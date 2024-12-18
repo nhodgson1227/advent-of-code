10d9f8b4f08e72d353324a818f80e24aa8cef194
Nate Hodgson <nhodgson1227@gmail.com>
2024-12-18 17:06:33 +0000

    2015d3p1 finished

--- 2015/day3/2015d3.py
+++ 2015/day3/2015d3.py
@@ -1,8 +1,33 @@
-# Open the file in read mode
-file = open('/workspace/advent-of-code/2015/day2/input.txt', 'r')
-for line in file:
-    
-# Close the file
-file.close()
+def openFile():
+    # Open the file in read mode
+    file = open('/workspace/advent-of-code/2015/day3/input.txt', 'r')
+    directions = file.read()
+    file.close()
+    return directions
 
-#
+directions = openFile()
+#directions = '^>v<^>v<'
+currentPos = [0,0]
+presents = {}
+
+for _dir in directions:
+    if _dir == '^':
+        currentPos[1] += 1
+    elif _dir == '>':
+        currentPos[0] += 1
+    elif _dir == '<':
+        currentPos[0] -= 1
+    elif _dir == 'v':
+        currentPos[1] -= 1
+    else:
+        print("Input error. Please check for valid input")
+    
+    # Check if the coordinates have been visited and increment a present delivered. If not, set them to 1 present delivered.
+    try:
+        presents[currentPos[0],currentPos[1]]
+    except:
+        presents[currentPos[0],currentPos[1]] = 1
+    else:
+        presents[currentPos[0],currentPos[1]] += 1
+
+print(len(presents))
